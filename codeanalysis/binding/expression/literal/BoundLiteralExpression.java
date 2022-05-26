0fc4cfa9317fac5a4d05d6ffb1670f95c8be85b4
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-05-26 12:57:07 -0300

    chore: literal bound expression type

---
+++ codeanalysis/binding/expression/literal/BoundLiteralExpression.java
@@ -0,0 +1,33 @@
+package codeanalysis.binding.expression.literal;
+
+import codeanalysis.binding.BoundNodeKind;
+import codeanalysis.binding.expression.BoundExpression;
+
+import java.lang.reflect.Type;
+
+public class BoundLiteralExpression extends BoundExpression {
+    private final Object value;
+    private final BoundNodeKind kind;
+    private final Type type;
+
+
+    public BoundLiteralExpression(Object value) {
+        this.value = value;
+        this.kind = BoundNodeKind.LITERAL_EXPRESSION;
+        this.type = value.getClass();
+    }
+
+    @Override
+    public BoundNodeKind getKind() {
+        return this.kind;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    @Override
+    public Type getType() {
+        return type;
+    }
+}
