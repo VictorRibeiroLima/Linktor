df6558c293020cca07070f3f3e368a3e741da11b
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-05-26 13:47:23 -0300

    fix: aways true if

--- codeanalysis/binding/Binder.java
+++ codeanalysis/binding/Binder.java
@@ -12,7 +12,17 @@ import codeanalysis.syntax.expression.ExpressionSyntax;
 import codeanalysis.syntax.expression.LiteralExpressionSyntax;
 import codeanalysis.syntax.expression.UnaryExpressionSyntax;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Binder {
+
+    private final List<String> diagnostics = new ArrayList<>();
+
+    public List<String> getDiagnostics() {
+        return diagnostics;
+    }
 
     public BoundExpression bindExpression(ExpressionSyntax syntax) throws Exception {
         switch (syntax.getKind()) {
@@ -34,18 +44,34 @@ public class Binder {
 
     private BoundExpression bindUnaryExpression(UnaryExpressionSyntax syntax) throws Exception {
         BoundExpression right = bindExpression(syntax.getRight());
-        BoundUnaryOperatorKind kind = binUnaryOperatorKind(syntax.getKind());
+        BoundUnaryOperatorKind kind = binUnaryOperatorKind(syntax.getKind(), right.getType());
+        if (kind == null) {
+            diagnostics.add(
+                    "ERROR: Unary operator " + syntax.getOperatorToken().getText() +
+                            " is not defined for type " + right.getType() + "."
+            );
+            return right;
+        }
         return new BoundUnaryExpression(kind, right);
     }
 
     private BoundExpression bindBinaryExpression(BinaryExpressionSyntax syntax) throws Exception {
         BoundExpression left = bindExpression(syntax.getLeft());
         BoundExpression right = bindExpression(syntax.getRight());
-        BoundBinaryOperatorKind kind = binBinaryOperatorKind(syntax.getKind());
+        BoundBinaryOperatorKind kind = binBinaryOperatorKind(syntax.getKind(), left.getType(), right.getType());
+        if (kind == null) {
+            diagnostics.add(
+                    "ERROR: Binary operator " + syntax.getOperatorToken().getText() +
+                            " is not defined for type " + left.getType() + " or " + right.getType() + "."
+            );
+            return left;
+        }
         return new BoundBinaryExpression(left, kind, right);
     }
 
-    private BoundUnaryOperatorKind binUnaryOperatorKind(SyntaxKind kind) throws Exception {
+    private BoundUnaryOperatorKind binUnaryOperatorKind(SyntaxKind kind, Type boundType) throws Exception {
+        if (!boundType.equals(Integer.class))
+            return null;
         switch (kind) {
             case PLUS_TOKEN:
                 return BoundUnaryOperatorKind.IDENTITY;
@@ -56,7 +82,9 @@ public class Binder {
         }
     }
 
-    private BoundBinaryOperatorKind binBinaryOperatorKind(SyntaxKind kind) throws Exception {
+    private BoundBinaryOperatorKind binBinaryOperatorKind(SyntaxKind kind, Type boundLeftType, Type boundRightType) throws Exception {
+        if (!boundLeftType.equals(Integer.class) || !boundRightType.equals(Integer.class))
+            return null;
         switch (kind) {
             case PLUS_TOKEN:
                 return BoundBinaryOperatorKind.ADDITION;
