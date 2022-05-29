9ee559167688baecb82fd5575d59d3fae0c0ed36
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-05-28 23:46:05 -0300

    test: parser unit test

--- src/test/java/codeanalysis/parser/ParserTest.java
+++ src/test/java/codeanalysis/parser/ParserTest.java
@@ -4,13 +4,15 @@ import codeanalysis.syntax.AssertingList;
 import codeanalysis.syntax.SyntaxFacts;
 import codeanalysis.syntax.SyntaxKind;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
 
@@ -68,6 +70,43 @@ class ParserTest {
                 asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
                     asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"c");
         }
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideBinaryOperatorPairs")
+    void parseParenthesizedExpressionHonorsPrecedence(SyntaxKind op1,SyntaxKind op2) {
+        int precedence1 = SyntaxFacts.getBinaryOperatorPrecedence(op1);
+        int precedence2 = SyntaxFacts.getBinaryOperatorPrecedence(op2);
+        String text1 = SyntaxFacts.getText(op1);
+        String text2 = SyntaxFacts.getText(op2);
+
+        String text = "(a "+text1+" b )"+text2+" c";
+
+        Parser parser = new Parser(text);
+        AssertingList asserting = new AssertingList(parser.parse().getRoot());
+        /*
+
+                  *
+                 / \
+                +  c
+               / \
+               a  b
+        */
+
+        asserting.assertNode(SyntaxKind.BINARY_EXPRESSION);
+            asserting.assertNode(SyntaxKind.PARENTHESIZED_EXPRESSION);
+                asserting.assertNode(SyntaxKind.OPEN_PARENTHESIS_TOKEN);
+                asserting.assertNode(SyntaxKind.BINARY_EXPRESSION);
+                    asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
+                        asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"a");
+                    asserting.assertToken(op1,text1);
+                    asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
+                        asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"b");
+                asserting.assertNode(SyntaxKind.CLOSE_PARENTHESIS_TOKEN);
+            asserting.assertToken(op2,text2);
+            asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
+                asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"c");
+
     }
 
     @ParameterizedTest
@@ -129,6 +168,68 @@ class ParserTest {
     }
 
     @ParameterizedTest
+    @MethodSource("provideBinaryUnaryOperatorPairs")
+    void parseAssignmentExpressionHonorsPrecedence(SyntaxKind binaryExpression,SyntaxKind unaryExpression){
+        String binaryText = SyntaxFacts.getText(binaryExpression);
+        String unaryText = SyntaxFacts.getText(unaryExpression);
+
+        String text = "a="+unaryText+"b "+binaryText+" c";
+
+        /*
+                   =
+                  / \
+                 a   +
+                     / \
+                    !   c
+                   /
+                  b
+
+        */
+
+        Parser parser = new Parser(text);
+        AssertingList asserting = new AssertingList(parser.parse().getRoot());
+
+        asserting.assertNode(SyntaxKind.ASSIGNMENT_EXPRESSION);
+            asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"a");
+            asserting.assertToken(SyntaxKind.EQUAL_TOKEN,"=");
+                asserting.assertNode(SyntaxKind.BINARY_EXPRESSION);
+                asserting.assertNode(SyntaxKind.UNARY_EXPRESSION);
+                asserting.assertToken(unaryExpression,unaryText);
+                asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
+                asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"b");
+                asserting.assertToken(binaryExpression,binaryText);
+                asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
+                asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"c");
+
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("provideAssignableKinds")
+    void parseAssignmentExpression(SyntaxKind expectedKind,String expectedText){
+        String text = "a="+expectedText;
+        Parser parser = new Parser(text);
+        AssertingList asserting = new AssertingList(parser.parse().getRoot());
+        asserting.assertNode(SyntaxKind.ASSIGNMENT_EXPRESSION);
+            asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"a");
+            asserting.assertNode(SyntaxKind.EQUAL_TOKEN);
+            asserting.assertNode(SyntaxKind.LITERAL_EXPRESSION);
+                asserting.assertToken(expectedKind,expectedText);
+    }
+
+    @Test
+    void parseBadParenthesizedExpression(){
+        String text = "(1";
+        Parser parser = new Parser(text);
+        AssertingList asserting = new AssertingList(parser.parse().getRoot());
+        asserting.assertNode(SyntaxKind.PARENTHESIZED_EXPRESSION);
+            asserting.assertNode(SyntaxKind.OPEN_PARENTHESIS_TOKEN);
+            asserting.assertNode(SyntaxKind.LITERAL_EXPRESSION);
+                    asserting.assertToken(SyntaxKind.NUMBER_TOKEN,"1");
+            asserting.assertNode(SyntaxKind.CLOSE_PARENTHESIS_TOKEN);
+    }
+
+    @ParameterizedTest
     @MethodSource("provideUnaryOperatorPairs")
     void parseUnaryUnaryExpression(SyntaxKind u1,SyntaxKind u2){
         String u1Text = SyntaxFacts.getText(u1);
@@ -154,6 +255,7 @@ class ParserTest {
                 asserting.assertNode(SyntaxKind.NAME_EXPRESSION);
                     asserting.assertToken(SyntaxKind.IDENTIFIER_TOKEN,"a");
     }
+
 
     static Stream<Arguments> provideBinaryOperatorPairs(){
         List<SyntaxKind> bos1 = SyntaxFacts.getBinaryOperatorKinds();
@@ -189,5 +291,14 @@ class ParserTest {
             }
         }
         return args.stream();
+    }
+
+    static Stream<Arguments> provideAssignableKinds(){
+        return Stream.of(
+                Arguments.of(SyntaxKind.NUMBER_TOKEN,"1"),
+                Arguments.of(SyntaxKind.NUMBER_TOKEN,"123"),
+                Arguments.of(SyntaxKind.FALSE_KEYWORD,"false"),
+                Arguments.of(SyntaxKind.TRUE_KEYWORD,"true")
+        );
     }
 }
