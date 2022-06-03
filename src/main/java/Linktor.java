ff97bc72c2abdf6b2f7a4de0258f06bef7b2a040
Victor Ribeiro de Lima <38430966+VictorRibeiroLima@users.noreply.github.com>
2022-06-03 02:08:02 -0300

    feat: add one bad file reader for tests (#15)

--- src/main/java/Linktor.java
+++ src/main/java/Linktor.java
@@ -7,14 +7,30 @@ import compilation.Compilation;
 import compilation.EvaluationResult;
 import util.ConsoleColors;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Linktor {
     public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                fromFile(args[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            repl();
+        }
+    }
+
+    private static void repl() {
         final String redColor = "\033[0;31m";
         final String whiteColor = "\033[0m";
         boolean showTree = false;
@@ -94,6 +110,43 @@ public class Linktor {
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("\033[0;31m" + e.getMessage());
+        }
+    }
+
+    private static void fromFile(String filePath) throws Exception {
+        File file = new File(filePath);
+        BufferedReader br
+                = new BufferedReader(new FileReader(file));
+
+        String input = br.lines().collect(Collectors.joining());
+
+
+        final Map<VariableSymbol, Object> variables = new HashMap<>();
+        SyntaxTree tree = SyntaxTree.parse(input);
+        Compilation compilation = new Compilation(tree);
+        EvaluationResult evaluationResult = compilation.evaluate(variables);
+        List<Diagnostic> diagnostics = evaluationResult.diagnostics();
+        Object result = evaluationResult.result();
+        if (diagnostics.isEmpty()) {
+            System.out.println(ConsoleColors.YELLOW_BRIGHT + "Result: " + result);
+        } else {
+            SourceText text = tree.getText();
+            for (Diagnostic diagnostic : diagnostics) {
+                int lineIndex = text.getLineIndex(diagnostic.span().start());
+                TextLine line = text.getLines().get(lineIndex);
+                int lineNumber = lineIndex + 1;
+                int character = diagnostic.span().start() - line.getStart() + 1;
+
+                System.out.println(ConsoleColors.RED);
+                System.out.println(diagnostic);
+
+                String prefix = input.substring(line.getStart(), diagnostic.span().start());
+                String error = tree.getText().toString(diagnostic.span());
+                String suffix = input.substring(diagnostic.span().end());
+
+                System.out.print("At Line(" + lineNumber + "," + character + "): ");
+                System.out.println(ConsoleColors.WHITE + prefix + ConsoleColors.RED + error + ConsoleColors.WHITE + suffix);
+            }
         }
     }
 }
