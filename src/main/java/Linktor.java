a8dd197126d42554abbca9bf9748c9b53348324b
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2022-06-03 21:52:48 -0300

    chore: improve the console app

--- src/main/java/Linktor.java
+++ src/main/java/Linktor.java
@@ -5,115 +5,31 @@ import codeanalysis.symbol.VariableSymbol;
 import codeanalysis.syntax.SyntaxTree;
 import compilation.Compilation;
 import compilation.EvaluationResult;
+import repl.LinktorRepl;
+import repl.Repl;
 import util.ConsoleColors;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Linktor {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            try {
-                fromFile(args[0]);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            repl();
-        }
-    }
-
-    private static void repl() {
-        final String redColor = "\033[0;31m";
-        final String whiteColor = "\033[0m";
-        boolean showTree = false;
-        boolean showProgram = false;
-        StringBuilder input = new StringBuilder();
-        Scanner console = new Scanner(System.in);
-        Compilation previous = null;
-        final Map<VariableSymbol, Object> variables = new HashMap<>();
         try {
-            while (true) {
-                if (input.isEmpty()) {
-                    System.out.print(">");
-                } else {
-                    System.out.print(".");
-                }
-                String inLineInput = console.nextLine();
-                switch (inLineInput) {
-                    case "#showTree" -> {
-                        showTree = !showTree;
-                        String isShowingTree = showTree ? "Showing parse tree" : "Not showing parse tree";
-                        System.out.println(isShowingTree);
-                        continue;
-                    }
-                    case "#reset" -> {
-                        previous = null;
-                        variables.clear();
-                        System.out.println("COMPILATION RESTARTED");
-                        continue;
-                    }
-                    case "#showProgram" -> {
-                        showProgram = !showProgram;
-                        String isShowingTree = showProgram ? "Showing program tree" : "Not showing program tree";
-                        System.out.println(isShowingTree);
-                        continue;
-                    }
-                }
+            if (args.length > 0) {
 
+                fromFile(args[0]);
 
-                input.append(inLineInput);
-                SyntaxTree tree = SyntaxTree.parse(input.toString());
-                if (!inLineInput.isEmpty() && !tree.getDiagnostics().isEmpty()) {
-                    input.append("\n");
-                    continue;
-                }
-
-                Compilation compilation = previous == null ? new Compilation(tree) : previous.continueWith(tree);
-                if (showTree) {
-                    tree.getRoot().writeTo(new PrintWriter(System.out, true));
-                }
-                if (showProgram) {
-                    compilation.emitTree(new PrintWriter(System.out, true));
-                }
-                EvaluationResult evaluationResult = compilation.evaluate(variables);
-                List<Diagnostic> diagnostics = evaluationResult.diagnostics();
-                Object result = evaluationResult.result();
-                if (diagnostics.isEmpty()) {
-                    System.out.println(ConsoleColors.YELLOW_BRIGHT + "Result: " + result);
-                    previous = compilation;
-                } else {
-                    SourceText text = tree.getText();
-                    for (Diagnostic diagnostic : diagnostics) {
-                        int lineIndex = text.getLineIndex(diagnostic.span().start());
-                        TextLine line = text.getLines().get(lineIndex);
-                        int lineNumber = lineIndex + 1;
-                        int character = diagnostic.span().start() - line.getStart() + 1;
-
-                        System.out.println(redColor);
-                        System.out.println(diagnostic);
-
-                        String prefix = input.substring(line.getStart(), diagnostic.span().start());
-                        String error = tree.getText().toString(diagnostic.span());
-                        String suffix = input.substring(diagnostic.span().end());
-
-                        System.out.print("At Line(" + lineNumber + "," + character + "): ");
-                        System.out.println(whiteColor + prefix + redColor + error + whiteColor + suffix);
-                    }
-                }
-                System.out.println(whiteColor + "-------");
-                input.setLength(0);
+            } else {
+                Repl repl = new LinktorRepl();
+                repl.run();
             }
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("\033[0;31m" + e.getMessage());
         }
     }
 
---
+++ src/main/java/repl/Repl.java
@@ -0,0 +1,52 @@
+package repl;
+
+import util.ConsoleColors;
+
+import java.util.Scanner;
+
+public abstract class Repl {
+
+    public void run() {
+        Scanner console = new Scanner(System.in);
+        try {
+            while (true) {
+                String submission = submission(console);
+                if (submission == null)
+                    break;
+                evaluate(submission);
+                System.out.println(ConsoleColors.WHITE + "-------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("\033[0;31m" + e.getMessage());
+        }
+    }
+
+    private String submission(Scanner console) {
+        StringBuilder input = new StringBuilder();
+        while (true) {
+            if (input.isEmpty()) {
+                System.out.print(">");
+            } else {
+                System.out.print(".");
+            }
+            String inLineInput = console.nextLine();
+            if (input.isEmpty() && inLineInput.isEmpty())
+                return null;
+            if (evaluateMetaCommand(inLineInput)) continue;
+
+            input.append(inLineInput);
+            if (isCompleteSubmission(inLineInput)) {
+                input.append("\n");
+                continue;
+            }
+            return input.toString();
+        }
+    }
+
+    protected abstract boolean evaluateMetaCommand(String inLineInput);
+
+    protected abstract void evaluate(String input) throws Exception;
+
+    protected abstract boolean isCompleteSubmission(String text);
+}
